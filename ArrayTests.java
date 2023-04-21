680be4f70d2ad8dc4624afd48b682d63a962fb7a
okaykei <energui@ucsd.edu>
2023-04-20 19:25:36 -0700

    fixed bugs

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,15 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+  @Test
+  public void testReverseInPlace2(){
+    int[] input2 = {1,2,3,4};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{4,3,2,1}, input2);
+  }
+  @Test
+  public void testReversed2() {
+    int[] input3 = {1,2,3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input3));
+  }
 }
